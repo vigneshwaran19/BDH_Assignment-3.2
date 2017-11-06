2f906781587178be91a9a855ed2b1364da18c00b
vigneshwaran19 <32232874+vigneshwaran19@users.noreply.github.com>
2017-11-07 01:56:11 +0530

    Add files via upload

---
+++ MapperOnly.java
@@ -0,0 +1,107 @@
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+
+//DRIVER CLASS
+public class MapperOnly 
+{
+
+  
+
+public static void main(String[] args) throws Exception 
+{
+    
+if (args.length != 2) 
+{
+     
+ System.err.println("Usage: <input path> <output path>");
+     
+ System.exit(-1);
+    
+}
+    
+	
+
+//Job Related Configurations
+	
+Configuration conf = new Configuration();
+	
+Job job = new Job(conf, "My mapper only job");
+	
+job.setJarByClass(MapperOnly.class);
+ 
+
+   
+    
+    // No reducer
+ 
+job.setNumReduceTasks(0);
+ 
+
+   
+    //Provide paths to pick the input file for the job
+
+FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+
+    
+    
+    //Provide paths to pick the output file for the job, and delete it if already present
+	
+Path outputPath = new Path(args[1]);
+	
+FileOutputFormat.setOutputPath(job, outputPath);
+	
+outputPath.getFileSystem(conf).delete(outputPath, true);
+
+
+
+    
+    //To set the mapper and reducer of this job
+    
+job.setMapperClass(TelesetMapper.class);
+
+
+    
+    
+   
+    
+    //set the input and output format class
+    
+job.setInputFormatClass(TextInputFormat.class);
+    
+job.setOutputFormatClass(TextOutputFormat.class);
+
+
+
+    //set up the output key and value classes 	
+    
+job.setOutputKeyClass(IntWritable.class);
+    
+job.setOutputValueClass(Text.class);
+
+
+    
+    //execute the job
+    
+System.exit(job.waitForCompletion(true) ? 0 : 1);
+  
+}
+
+}
---
+++ TelesetMapper.java
@@ -0,0 +1,61 @@
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.hadoop.io.NullWritable;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+import java.io.IOException;
+
+import java.util.*;
+
+
+//MAPPER CLASS
+
+
+
+
+public class TelesetMapper extends Mapper <LongWritable, Text, NullWritable, Text>
+
+
+{
+
+public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException
+
+
+{
+
+
+final Text word = new Text();
+
+NullWritable a= NullWritable.get();
+
+String line=value.toString();
+
+StringTokenizer iterator = new StringTokenizer(line,"\\|");
+
+String company = iterator.nextToken();
+
+String product = iterator.nextToken();
+
+if(company.matches("NA") | product.matches("NA"))
+
+{
+	
+	
+}
+
+else
+{
+	
+word.set(line);
+
+context.write(a,word);
+
+}
+
+}
+
+}
